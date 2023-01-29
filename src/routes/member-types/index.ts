d44499bffdfac8889506da38feeb6f888c2ec27b
Jevgenijs Kozlovs <unstoo@gmail.com>
2023-01-29 22:10:04 +0200

    feat: add membertypes rest endpoint

--- src/routes/member-types/index.ts
+++ src/routes/member-types/index.ts
@@ -1,6 +1,6 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-// import { idParamSchema } from '../../utils/reusedSchemas';
-// import { changeMemberTypeBodySchema } from './schema';
+import { idParamSchema } from '../../utils/reusedSchemas';
+import { changeMemberTypeBodySchema } from './schema';
 import type { MemberTypeEntity } from '../../utils/DB/entities/DBMemberTypes';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
@@ -10,28 +10,44 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
     return fastify.db.memberTypes.findMany();
   });
 
-  // fastify.get(
-  //   '/:id',
-  //   {
-  //     schema: {
-  //       params: idParamSchema,
-  //     },
-  //   },
-  //   async function (request, reply): Promise<MemberTypeEntity> {
-  //     const memberTypes = fastify.db.memberTypes.findOne({ key: 'id', equals: request.params.id });
-  //   }
-  // );
+  fastify.get(
+    '/:id',
+    {
+      schema: {
+        params: idParamSchema,
+      },
+    },
+    async function (request, reply): Promise<MemberTypeEntity | null> {
+      const memberType = await fastify.db.memberTypes.findOne({ key: 'id', equals: request.params.id });
 
-  // fastify.patch(
-  //   '/:id',
-  //   {
-  //     schema: {
-  //       body: changeMemberTypeBodySchema,
-  //       params: idParamSchema,
-  //     },
-  //   },
-  //   async function (request, reply): Promise<MemberTypeEntity> {}
-  // );
+      if (!memberType) {
+        reply.statusCode = 404;
+        return null;
+      }
+
+      return memberType;
+    }
+  );
+
+  fastify.patch(
+    '/:id',
+    {
+      schema: {
+        body: changeMemberTypeBodySchema,
+        params: idParamSchema,
+      },
+    },
+    async function (request, reply): Promise<MemberTypeEntity | null> {
+      const memberType = await fastify.db.memberTypes.findOne({ key: 'id', equals: request.params.id });
+
+      if (!memberType) {
+        reply.statusCode = 400;
+        return null;
+      }
+
+      return await fastify.db.memberTypes.change(request.params.id, request.body);
+    }
+  );
 };
 
 export default plugin;
